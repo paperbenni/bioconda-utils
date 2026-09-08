d9bea352be6cd6b36a5e7c293b178d375b481453
paperbenni <paperbenni@gmail.com>
2026-09-08 11:18:18 +0000

    chore: remove unused dependencies

--- bioconda_utils/utils.py
+++ bioconda_utils/utils.py
@@ -49,7 +49,6 @@ import requests
 import tqdm as _tqdm
 import yaml
 import zstandard
-from boltons.funcutils import FunctionBuilder
 from colorlog import ColoredFormatter
 from conda_build import api
 from github import Github
@@ -155,44 +154,6 @@ def ensure_list(obj):
     if isinstance(obj, Sequence) and not isinstance(obj, str):
         return obj
     return [obj]
-
-
-def wraps(func, hide_wrapped=False):
-    """Custom wraps() function for decorators
-
-    This one differs from functiools.wraps and boltons.funcutils.wraps in
-    that it allows *adding* keyword arguments to the function signature.
-
-    >>> def decorator(func):
-    >>>   @wraps(func)
-    >>>   def wrapper(*args, extra_param=None, **kwargs):
-    >>>      print("Called with extra_param=%s" % extra_param)
-    >>>      func(*args, **kwargs)
-    >>>   return wrapper
-    >>>
-    >>> @decorator()
-    >>> def test(arg1, arg2, arg3='default'):
-    >>>     pass
-    >>>
-    >>> test('val1', 'val2', extra_param='xyz')
-    """
-
-    fb = FunctionBuilder.from_func(func)
-
-    def wrapper_wrapper(wrapper_func):
-        fb_wrapper = FunctionBuilder.from_func(wrapper_func)
-        fb.kwonlyargs += fb_wrapper.kwonlyargs
-        fb.kwonlydefaults.update(fb_wrapper.kwonlydefaults)
-        fb.body = f"return _call({fb.get_invocation_str()})"
-        execdict = {"_call": wrapper_func, "_func": func}
-        fully_wrapped = fb.get_func(execdict)
-        if not hide_wrapped:
-            fully_wrapped.__wrapped__ = func
-        elif hasattr(fully_wrapped, "__wrapped__"):
-            del fully_wrapped.__dict__["__wrapped__"]
-        return fully_wrapped
-
-    return wrapper_wrapper
 
 
 class LogFuncFilter:
